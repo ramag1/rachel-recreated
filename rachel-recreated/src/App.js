bdb838c06454e190d8cba748fb4518a1b8944a25
rmagley <rachelmagley@gmail.com>
2022-03-30 10:25:15 -0700

    First commit

---
+++ rachel-recreated/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Notes On Little Things</h1>
+    </div>
+  );
+}
+
+export default App;
